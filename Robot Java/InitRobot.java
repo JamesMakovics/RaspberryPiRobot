a800e8e203c11e07cee4df204a71bbd75f89c3b3
JamesMakovics <jmakovics1@gmail.com>
2018-04-04 19:37:14 -0400

    Missing a semi colon

--- Robot Java/InitRobot.java
+++ Robot Java/InitRobot.java
@@ -2,7 +2,7 @@
 
 public class InitRobot extends java.lang.Object{
     public InitRobot(){
-        leftMotorForward = new Motor(27)
+        leftMotorForward = new Motor(27);
         leftMotorBackward = new Motor(17);
         rightMotorForward = new Motor(22);
         rightMotorBackward = new Motor(23);
