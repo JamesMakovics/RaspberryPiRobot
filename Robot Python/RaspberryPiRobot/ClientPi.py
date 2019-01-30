fb24330c8596eef1644c057c307b0c2ea8de7a01
James <jmakovics1@gmail.com>
2019-01-29 22:12:41 -0500

    Fixed cam server host code to send instead of receive Added parallel functions for cam and command servers and clients

--- Robot Python/RaspberryPiRobot/ClientPi.py
+++ Robot Python/RaspberryPiRobot/ClientPi.py
@@ -5,9 +5,10 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 import socket
-import runMotors
-import robotControlMethods
+import setMotors
+import robotControllerMethods
 import pygame
+import pygame.camera
 
 #inits pygame with the correct video configs
 pygame.init()
@@ -18,23 +19,50 @@ cam.start()
 #creates a video feed
 image = cam.get_image()
 print = cam.get_size()
-img_str = pygame.image.tostring(img,"RGB")
+img_str = pygame.image.tostring(image,"RGB")
 
 UDP_IP = "192.168.1.125" #Home ip 192.168.1.30
 UDP_PORT = 5005
 address = UDP_IP,UDP_PORT
 
+p1 = Process(target=startCamServer)
+  p1.start(UDP_IP, UDP_PORT)
+  p2 = Process(target=startCommandServer)
+  p2.start(UDP_IP, UDP_PORT)
+  p1.join(UDP_IP, UDP_PORT)
+  p2.join(UDP_IP, UDP_PORT)
+
 #creates a video server and attempts to send it over UDP
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect((UDP_IP,UDP_PORT))
-s.sendall(img_str)
-s.close
+def startCamServer(UDP_IP, UDP_PORT):
+    s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((UDP_IP,UDP_PORT))
+    s.listen(1)
+    conn, addr = s.accept()
+
+    string = bytes('','UTF-8')
+
+
+    while True:
+
+        d = conn.recv(640*480)
+
+        if not d:
+            print ("break")
+            break
+
+        else:
+
+            string += d
+
+            pil_image = Image.fromstring("RGB",(352,288),string)
+    #(352,288) is the return of cam.get_size()
 #new section added above test if this works later!
 
 #gets controls over UDP from client
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
-while True:
-    move, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    move = move.decode(encoding='UTF-8',errors='strict')
-    setMotors.getDriveCommand(move)
+def startCommandServer(UDP_IP, UDP_PORT):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+    sock.bind((UDP_IP, UDP_PORT))
+        while True:
+            move, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+            move = move.decode(encoding='UTF-8',errors='strict')
+            setMotors.getDriveCommand(move)
--- Robot Python/RaspberryPiRobot/pyGameControl.py
+++ Robot Python/RaspberryPiRobot/pyGameControl.py
@@ -18,23 +18,28 @@ print("Right Arrow -> moves robot right")
 UDP_IP = "192.168.1.125" #This is the ip of the Pi School ip: 10.120.98.209
 UDP_PORT = 5005 #This is the port it connects over
 address = UDP_IP, UDP_PORT
+
+p1 = Process(target=startCamClient)
+  p1.start(UDP_IP, UDP_PORT)
+  p2 = Process(target=startCommandClient)
+  p2.start(UDP_IP, UDP_PORT)
+  p1.join(UDP_IP, UDP_PORT)
+  p2.join(UDP_IP, UDP_PORT)
+
 #new camera stream
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((host,port))
-s.listen(1)
-conn, addr = s.accept()
+def startCamClient(UDP_IP, UDP_PORT):
+    s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((UDP_IP,UDP_PORT))
+    s.listen(1)
+    conn, addr = s.accept()
 
-string = bytes('','UTF-8')
-#new part between the comments come back and test later
+    string = bytes('','UTF-8')
+    #new part between the comments come back and test later
 
-sock = socket.socket(socket.AF_INET, # Internet
+    sock = socket.socket(socket.AF_INET, # Internet
                  socket.SOCK_DGRAM) # UDP
-
-try:
     while True:
-        events = pygame.event.get()
 
-        #gets camera feed must test later
         d = conn.recv(640*480)
 
         if not d:
@@ -46,31 +51,35 @@ try:
             string += d
 
         pil_image = Image.fromstring("RGB",(352,288),string)
-        #(352,288) is the return of cam.get_size()
-        #gets camera feed and displays it must test later
+    #(352,288) is the return of cam.get_size()
 
-        for event in events:
-            if event.type == pygame.QUIT:
-                GPIO.cleanup()
-                pygame.display.quit()
-                pygame.quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE: #Quits the program
-                    break
-                elif event.key == pygame.K_UP: #Moves Robot forward
-                    move = "Forward"
+def startCommandClient(UDP_IP, UDP_PORT):
+    try:
+        while True:
+            events = pygame.event.get()
+
+            for event in events:
+                if event.type == pygame.QUIT:
+                    GPIO.cleanup()
+                    pygame.display.quit()
+                    pygame.quit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE: #Quits the program
+                        break
+                    elif event.key == pygame.K_UP: #Moves Robot forward
+                        move = "Forward"
+                        sock.sendto(str(move).encode('utf-8'), address)
+                    elif event.key == pygame.K_DOWN: #Moves Robot Backwards
+                        move = "Backward"
+                        sock.sendto(str(move).encode('utf-8'), address)
+                    elif event.key == pygame.K_LEFT:
+                        move = "Right"
+                        sock.sendto(str(move).encode('utf-8'), address)
+                    elif event.key == pygame.K_RIGHT:
+                        move = "Left"
+                        sock.sendto(str(move).encode('utf-8'), address)
+                else:
+                    move = "Stop"
                     sock.sendto(str(move).encode('utf-8'), address)
-                elif event.key == pygame.K_DOWN: #Moves Robot Backwards
-                    move = "Backward"
-                    sock.sendto(str(move).encode('utf-8'), address)
-                elif event.key == pygame.K_LEFT:
-                    move = "Right"
-                    sock.sendto(str(move).encode('utf-8'), address)
-                elif event.key == pygame.K_RIGHT:
-                    move = "Left"
-                    sock.sendto(str(move).encode('utf-8'), address)
-            else:
-                move = "Stop"
-                sock.sendto(str(move).encode('utf-8'), address)
-finally:
-    GPIO.cleanup()
+    finally:
+        GPIO.cleanup()
